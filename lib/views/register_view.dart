d6be666bebc08d7d74e0afba69c12d4fd8895036
Patrick Kratzer <sparksman@web.de>
2022-03-09 10:07:25 +0100

    Add routes

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -75,6 +75,13 @@ class _RegisterViewState extends State<RegisterView> {
                     },
                     child: const Text('Register'),
                   ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context)
+                          .pushNamedAndRemoveUntil('/login/', (route) => false);
+                    },
+                    child: const Text('Already registered? Login here!'),
+                  )
                 ],
               );
             default:
